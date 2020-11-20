a000f817619a60a0879b490b95943a275f523545
Justice Mabuza <jmabuza@my365-uwc.ac.za>
2020-11-20 08:04:48 +0200

    Add project files.

---
+++ MyAssignment1/MyAssignment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+namespace Assignment1.p1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputString = "I submited this assignment a long time go";
+            string reversedString = string.Empty;
+
+            for (int counter = inputString.Length - 1; counter >= 0; counter--)
+            {
+                reversedString = reversedString + inputString[counter];
+
+            }
+            Console.WriteLine(reversedString);
+
+        }
+    }
+}
---
+++ MyAssignment1mis/MyAssignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
